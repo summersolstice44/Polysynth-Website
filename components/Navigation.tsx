9e2608e92457b450a0d45e789c3867ebf50bdd7e
summersolstice44 <cnickel2000@gmail.com>
2026-01-07 16:01:44 -0800

    trying to fix letters being clipped on animated logo

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -16,19 +16,19 @@ export default function Navigation() {
     { href: '/contact', label: 'Contact' },
   ]
 
-  // Letter positions around the circle - adjusted to be closer to logo
+  // Letter positions around the circle - scaled down for compact header
   const letters = [
-    { char: 'p', transform: 'translate(50px) rotate(90deg)' },
-    { char: 'o', transform: 'translate(42px, 27px) rotate(122.727deg)' },
-    { char: 'l', transform: 'translate(21px, 46px) rotate(155.455deg)' },
-    { char: 'y', transform: 'translate(-7px, 50px) rotate(188.182deg)' },
-    { char: ' ', transform: 'translate(-33px, 38px) rotate(220.909deg)' },
-    { char: 's', transform: 'translate(-49px, 14px) rotate(253.636deg)' },
-    { char: 'y', transform: 'translate(-49px, -14px) rotate(286.364deg)' },
-    { char: 'n', transform: 'translate(-33px, -38px) rotate(319.091deg)' },
-    { char: 't', transform: 'translate(-7px, -50px) rotate(351.818deg)' },
-    { char: 'h', transform: 'translate(21px, -46px) rotate(384.545deg)' },
-    { char: ' ', transform: 'translate(42px, -27px) rotate(417.273deg)' },
+    { char: 'p', transform: 'translate(35px) rotate(90deg)' },
+    { char: 'o', transform: 'translate(29px, 19px) rotate(122.727deg)' },
+    { char: 'l', transform: 'translate(15px, 32px) rotate(155.455deg)' },
+    { char: 'y', transform: 'translate(-5px, 35px) rotate(188.182deg)' },
+    { char: ' ', transform: 'translate(-23px, 27px) rotate(220.909deg)' },
+    { char: 's', transform: 'translate(-34px, 10px) rotate(253.636deg)' },
+    { char: 'y', transform: 'translate(-34px, -10px) rotate(286.364deg)' },
+    { char: 'n', transform: 'translate(-23px, -27px) rotate(319.091deg)' },
+    { char: 't', transform: 'translate(-5px, -35px) rotate(351.818deg)' },
+    { char: 'h', transform: 'translate(15px, -32px) rotate(384.545deg)' },
+    { char: ' ', transform: 'translate(29px, -19px) rotate(417.273deg)' },
   ]
 
   return (
@@ -53,8 +53,8 @@ export default function Navigation() {
               <Image
                 src="/logo.png"
                 alt="Polysynth Logo"
-                width={100}
-                height={100}
+                width={60}
+                height={60}
                 priority
               />
             </div>
